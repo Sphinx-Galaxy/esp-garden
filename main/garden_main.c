fdd4e438b563d26f253f1299aa83df06ca717c75
Mattis Jaksch <Mattis.Jaksch@dlr.de>
2022-03-04 20:57:57 +0100

    Added full alphabet

--- main/garden_main.c
+++ main/garden_main.c
@@ -17,16 +17,18 @@ void app_main(void)
   	
   	display_clear(dp);
   	
-  	display_write(dp, "MATTIS");
-  	display_write(dp, "TEST\n");
+  	display_write(dp, "01234569\n");
+  	display_write(dp, "ABCDEFGHI\n");
+  	display_write(dp, "JKLMNOPQR\n");
+  	display_write(dp, "STUVWXYZ\n");
   	
-  	char res[32];
+  	/*char res[32];
   	
-  	for(int i = 0; i < 32; ++i)
+  	for(int i = 0; i < 8; ++i)
   	{
-  		sprintf(res, "TESAT%d", i);
+  		sprintf(res, "MATTIS%d\n", i);
   		display_write(dp, res);
-  	}
+  	}*/
   	
   	display_destroy(dp);
 }
